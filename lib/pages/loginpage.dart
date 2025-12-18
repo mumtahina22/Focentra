e84f1d967b3258210a9f5f4022c4fbd2a7e1f855
Mumtahina Islam Khan <mumtahinaislamkhan@gmail.com>
2025-12-18 23:58:29 +0600

    Your commit message here

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -40,7 +40,7 @@ class _LoginPageState extends State<LoginPage> {
           onPressed: () => Navigator.pop(context),
         ),
         title: Text(
-          'ZenStudy',
+          'ZFocentra',
           style: TextStyle(
             color: colorScheme.onSurface,
             fontSize: 20,
